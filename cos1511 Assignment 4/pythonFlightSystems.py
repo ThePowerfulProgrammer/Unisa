9d903a88177891fd764edb4ae6ccaf3ef3664c1a
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-07-11 15:35:38 +0200

    COS1511 Python booking system

---
+++ cos1511 Assignment 4/pythonFlightSystems.py
@@ -0,0 +1,34 @@
+flight_route = "JHB CPT"
+flight_time = 2.5
+total_seats = 50
+'''
+Option Departure Time Arrival Time
+1      7:00           9:30
+2      9:00           11:30
+3      11:00          13:30
+4      13:00          15:30
+5      15:00          17:30
+'''
+
+# Seating arrangement
+'''
+First class(1920.00)
+|A1|A2|A3|----|A4|A5|A6|
+B..
+C..
+D..
+Economy class(1600.00)
+|E1|E2|E3|----|E4|E5|E6|
+F..
+G..
+H..
+|I1|I2|
+'''
+
+# Begin
+print("Welcome to COS1511 Flight Booking system")
+# Prompt fullname
+print("Enter fullname ")
+name = input("")
+
+print(name)
