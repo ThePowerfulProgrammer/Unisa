a9bd651107838ab14cfde0a0b652f3e4ef57b941
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-07-20 12:00:09 +0200

    Next psuh

--- cos1511 Assignment 4/pythonFlightSystems.py
+++ cos1511 Assignment 4/pythonFlightSystems.py
@@ -15,12 +15,29 @@ class flight:
                      15:{43:"H1",44:"H2",45:"H3"},16:{46:"H4",47:"H5",48:"H6"},
                      17:{49:"I1",50:"I2"}
                      }
-
+    
+    def get_seat(self):
+        count = 1
+        for dic in self.seats.values():
+            for seatN in dic:
+                print(dic[seatN], end="|")
+                if (count == 3 or count == 9 or count == 15 or count == 21 or count == 27 or count == 33 or count == 39 or 
+                    count == 45): 
+                    print("---", end="|")
+                if (count == 6 or count == 12 or count  == 18 or count == 24 or count == 30 or count  == 36 
+                   or count == 42 or count == 48):
+                    print()
+                count += 1                
+        return ""
+        
     def book_seat(self, seat_number):
         for dictionary in self.seats.values():    
             for seatN in dictionary:
                 if (seat_number == dictionary[seatN]):
                     dictionary[seatN] = "**"
+                
+
+    
 
 flight1 = flight("7:00")
 flight2 = flight("9:00")
@@ -39,25 +56,37 @@ def flights_available():
     return 0
 
 # begin
-print("Welcome to COS1511 Flight Booking system")
-print("Enter fullname ")
-name = input("")
+start = 1
+while (start != 0):
+    print("Welcome to COS1511 Flight Booking system")
+    print("Enter fullname ")
+    name = input("")
 
-# 1st available flight times
-print("The available travel travel times for flights are ")
-flights_available()
+    # 1st available flight times
+    print("The available travel travel times for flights are ")
+    flights_available()
 
-# Choose flight times
-print("Choose the time by entering the option number from the given list")
-flight_number = int(input(""))
-while (flight_number < 1 or flight_number > 5):
-    print("Incorrect option! Please choose from 1-5")
+    # Choose flight times
+    print("Choose the time by entering the option number from the given list")
     flight_number = int(input(""))
+    while (flight_number < 1 or flight_number > 5):
+        print("Incorrect option! Please choose from 1-5")
+        flight_number = int(input(""))
 
-# Issue 5 flights all with mutabilty -> Must be able to book seats using ** 
-# Create a flight object? --> Classes
-print("Please key in a seat number to choose a seat(eg:A2)")
-seat_number = input()
-flight1.book_seat(seat_number)
-
-print(flight1.seats)
+    # Issue 5 flights all with mutabilty -> Must be able to book seats using ** 
+    # Create a flight object? --> Classes
+    print("Please key in a seat number to choose a seat(eg:A2)")
+    seat_number = input()
+    if (flight_number == 1):
+        flight1.book_seat(seat_number)
+    elif (flight_number == 2):
+        flight2.book_seat(seat_number)
+    elif (flight_number == 3):
+        flight3.book_seat(seat_number)
+    elif (flight_number == 4):
+        flight4.book_seat(seat_number)
+    else:
+        flight5.book_seat(seat_number)
+        
+    print("Would you like to book another seat? 1 for yes 0 for no ")
+    start = int(input())
