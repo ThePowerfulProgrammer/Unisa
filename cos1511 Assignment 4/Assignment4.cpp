6e2323bf21770e3e863f5ae2d021744e16607743
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-08-06 16:41:51 +0200

    c++ push

---
+++ cos1511 Assignment 4/Assignment4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// travelTimes
+void travelTimes(string departures[], string arrivals[]) {
+    
+}
+
+// main Program 
+string mainProgram() {
+    string name;
+    string surname;
+
+    cout << "Enter full name" << endl;
+    cin >> name >> surname ;
+
+    cout << "The available travel times for flights are " << endl;
+    // travel times function
+
+    return "";
+}
+
+int main() {
+    // allowed global vars + size of any other arrays
+    float economyTicket = 1600.00;
+    string arrivals[5] = {"7:00", "9:00", "11:00", "13:00", "15:00"};
+    string departures[5] = {"9:30","11:30", "13:30", "15:30", "17:30"};
+
+    cout << "Welcome to COS1511 Flight Booking system " << endl << endl;
+    
+
+}
