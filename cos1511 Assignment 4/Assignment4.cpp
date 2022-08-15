a3dc8f0f83b27222ef182c27f0f5dc9e754c7a38
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-08-15 19:24:55 +0200

    Complete with 0 checks

--- cos1511 Assignment 4/Assignment4.cpp
+++ cos1511 Assignment 4/Assignment4.cpp
@@ -41,10 +41,13 @@ void addSeats(Flight & flight) {
     }
 }
 
+// displays seats A1-I2 WORKS pre booking and post booking by making use of an additional array
 void displaySeats(Flight & flight) {
     cout << "First class(1920.00)" << endl;
+    
     for (int i=0;i<50;i++) {
         if (i == 24) {cout << "Economy class(1600.00)" << endl;}
+        
         cout << "|" << flight.seats[i] << "|";
         if (i+1 == 3 || i+1 == 9 || i+1 == 15 || i+1 == 21 || i+1 == 27 || i+1 == 33 || i+1 == 39 || i+1 == 45) {
             cout << "---" ;
@@ -52,28 +55,13 @@ void displaySeats(Flight & flight) {
         if (i == 5 || i == 11 || i == 17 || i == 23 || i == 29 || i == 35 || i == 41 || i == 47) {
             cout << endl;
         }
+        
     }
+    
 }
 
-// display flighttimes
-void displayFlightTimes(string flightTimes[][2]) {
-    cout << "The available travel times for flights are " << endl;
-    // travel times --> does nothing
-    cout << "    Depart   Arrive" << endl;
 
-
-    for (int i = 0; i<5;i++) {
-        if (i < 2) {
-            cout << i+1 << ".  " << flightTimes[i][0] << "     " << flightTimes[i][1] << endl;
-        }
-        else {
-            cout << i+1 << ".  " << flightTimes[i][0] << "    " << flightTimes[i][1] << endl;
-        }
-    }
-    cout << endl;
-
-}
-
+// function to choose flight -> flight1-flight5 -> validates user input on flight number 
 int bookFlight(int & flightTime) {
     cout << "Choose the correct time by entering the option from the displayed list:" << endl;
     // validation:
@@ -85,6 +73,7 @@ int bookFlight(int & flightTime) {
     return flightTime;
 }
 
+// functions that works with bookSeat function -> returns a value used to validate chosen seat
 bool checkBookedSeats(Flight & flight, string & seat) {
     for (int i = 0;i<50;i++) {
         if (seat == flight.bookedSeats[i]) {
@@ -95,9 +84,10 @@ bool checkBookedSeats(Flight & flight, string & seat) {
    
 }
 
+// function to validate and book seat based on user input
 void bookSeat(Flight & flight) {
     // get seat
-    int n=0; // used to add to booked 
+    int n=0; // used to add to booked array
     string seat;
     cout << "Please key in a seat number to choose a seat(eg:A2)" << endl;
     cin >> seat;
@@ -107,7 +97,7 @@ void bookSeat(Flight & flight) {
         cin >> seat;
     }
     
-    // if not ** --> Book seat
+    // if not ** --> Book seat, add cost of flight, set travelClass, and add 1 to number of bookings 
     for (int i = 0; i<50;i++) {
         if (seat == flight.seats[i]) {
             flight.seats[i] = "**";
@@ -123,11 +113,12 @@ void bookSeat(Flight & flight) {
             flight.bookings += 1;
         }
     }
-    // added to bookedSeats
+    // add seat to bookedSeats
     flight.bookedSeats[n] = seat;
     n +=1;
 }
 
+// function to displayTicket reduces redundancy -> needs work
 void displayTicket(string & name, string & surname, Flight & flight, string flightTimes[][2], int & flightTime) {
     cout << "**************************" << endl;
     cout << "Travel ticket for FLIGHT " << endl;
@@ -159,6 +150,22 @@ void displayTicket(string & name, string & surname, Flight & flight, string flig
 
 }
 
+// display flighttimes using nested array
+void displayFlightTimes(string flightTimes[][2]) {
+    cout << "The available travel times for flights are " << endl;
+    cout << "    Depart   Arrive" << endl;
+
+    for (int i = 0; i<5;i++) {
+        if (i < 2) {
+            cout << i+1 << ".  " << flightTimes[i][0] << "     " << flightTimes[i][1] << endl;
+        }
+        else {
+            cout << i+1 << ".  " << flightTimes[i][0] << "    " << flightTimes[i][1] << endl;
+        }
+    }
+    cout << endl;
+
+}
 
 // main Program 
 string mainProgram(string flightTimes[][2]) {
@@ -168,6 +175,7 @@ string mainProgram(string flightTimes[][2]) {
     string name;
     string surname;
     int flightTime;
+    // create 5 flight objects
     Flight flight1;
     Flight flight2;
     Flight flight3;
